8f25f9901d0b9a51071926815fe546cddc195878
Shu(アクア) <92246611+Shu343@users.noreply.github.com>
2022-09-06 09:32:57 +0530

    Update uploader.py

--- main/modules/uploader.py
+++ main/modules/uploader.py
@@ -25,12 +25,6 @@ async def upload_video(msg: Message,file,id,tit,name,ttl):
             ep_num = get_epnum(name)
             thumbnail,w,h = await generate_thumbnail(id,file,tit,ep_num,size,format_time(duration))
             tags = tags_generator(tit)
-            buttons = InlineKeyboardMarkup([
-                [
-                    InlineKeyboardButton(text="Info", url="https://t.me/Anime_Dex"),
-                    InlineKeyboardButton(text="Comments", url=f"https://t.me/+4nUo4jBR-JgxMTVl")
-                ]
-            ])
             caption = f"🎥 **{name}**\n\n{tags}"
             x = await app.send_video(
                 UPLOADS_ID,
